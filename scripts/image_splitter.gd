d1206bfa86d9c6f454a674232600ea5175d12dd7
poses98 <posesand@gmail.com>
2024-10-28 17:35:42 +0100

    Added a line when user select stars. Fixed x=0 issue when processing edges.

--- scripts/image_splitter.gd
+++ scripts/image_splitter.gd
@@ -10,6 +10,7 @@ var edgePoints = []
 var draw_image: Image;
 var rng = RandomNumberGenerator.new()
 var constelation_node:Node2D
+var constellation_list=[]
 
 func _ready() -> void:
 	if !image:
@@ -22,51 +23,51 @@ func _ready() -> void:
 		instantiate_stars()
 		printEdges()
 		
-
-
 func _process(delta: float) -> void:
 	$Label.text = get_stars_remaining()
 	
-
+# Fills the edges array depending of the given image
 func process_edges():
+	edgePoints = []
+	
+	# Time measurement and opt pixels
 	var start_time = Time.get_ticks_msec()  # Tiempo de inicio
-	var l_found = false
-	var r_found = false
-	var reverse_adjustment = 0;
 	var pixels_saved=0
+	
+	var l_found = false
+	var reverse_adjustment = 0;
+	
 	# Create a list with the edge of the image
-	for i in range(size.y):
-		for j in range(size.x):
-			if !l_found and j - 1 >= 0:
-				if isEmptyPixel(Vector2i(j-1, i)) and !isEmptyPixel(Vector2i(j, i)):
+	for i in range(size.y): # i is the y coordinate!
+		for j in range(size.x): # j is the x !
+			if !l_found and (j - 1 >= 0 or j == 0):
+				if (j == 0 and !isEmptyPixel(Vector2i(j,i))) or (isEmptyPixel(Vector2i(j-1, i)) and !isEmptyPixel(Vector2i(j, i))):
 					l_found = true
 					edgePoints.append(Vector2(j, i))
 					for k in range(size.x - 1, j, -1):
 						if k + 1 < size.x and !isEmptyPixel(Vector2i(k, i)) and isEmptyPixel(Vector2i(k+1, i)):
-							r_found = true
 							pixels_saved += k - j 
 							edgePoints.append(Vector2(k, i))
 							break
 						elif k == size.x-1:
 							if !isEmptyPixel(Vector2i(k,i)):
-								r_found = true
 								pixels_saved += k - j 
 								edgePoints.append(Vector2(k, i))
 								break	
 					break 
-			elif j == 0:
-				if !isEmptyPixel(Vector2i(j,i)):
-					edgePoints.append(Vector2(j,i))
-					l_found=true
 		l_found = false
-		r_found = false
 	var end_time = Time.get_ticks_msec()  # Tiempo de finalización
 	print("Tiempo de ejecución optimizado (ms): ", end_time - start_time)
 	print("Píxeles ahorrados: ", pixels_saved)
+	print("Pixel/ms: ", ((size.x*size.y)-pixels_saved)/(end_time-start_time))
 
 func isEmptyPixel(position:Vector2i):
-	return image.get_image().get_pixel(position.x,position.y).a == 0
+	if position.x < 0 or position.y > size.y - 1:
+		return -1
+	else:
+		return image.get_image().get_pixel(position.x,position.y).a == 0
 
+# Instantiates every star depending on the edgePoints array and the other stars.
 func instantiate_stars():
 	var canvas_size = get_viewport_rect().size
 	var scale_factor_x = canvas_size.x / size.x / 4
@@ -74,23 +75,27 @@ func instantiate_stars():
 	var count = 0
 	$ConstelationContainer.position= Vector2(rng.randi_range(0+canvas_size.x/6, canvas_size.x-canvas_size.x/6), rng.randi_range(0+canvas_size.y/4, canvas_size.y-canvas_size.y/2))
 	for point in edgePoints:
-		if point.y == edgePoints[0].y || point.y == edgePoints[edgePoints.size()-1].y || point.y == image.get_height()/2 || point.y == image.get_height()/2 + image.get_height()/4 || point.y == image.get_height()/2 - image.get_height()/4:
+		if point.y == edgePoints[0].y || point.y == edgePoints[edgePoints.size()-1].y || point.y == roundi(image.get_height()/2) || point.y == roundi(image.get_height()/2) + roundi(image.get_height()/4) || point.y == roundi(image.get_height()/2) - roundi(image.get_height()/4):
 			var star_instance = star_scene.instantiate()
 			$ConstelationContainer.add_child(star_instance)
 			star_instance.position = Vector2(point.x * scale_factor_x, point.y * scale_factor_y)
+			star_instance.connect("select", _on_star_selectable_select)
+			
 		count = count + 1
 	$ConstelationContainer.rotate(rng.randf_range(-3.14,3.14))
 	var star_out = true;
-	#TODO check if there are any stars out of the canvas, if so rng the $ConstelationContainer.position again
+	# Reallocating the whole constelation in the canvas if a star is out of bounds
 	while star_out:
 		star_out = false
 		$ConstelationContainer.position= Vector2(rng.randi_range(0+canvas_size.x/10, canvas_size.x-canvas_size.x/10), rng.randi_range(0+canvas_size.y/10, canvas_size.y-canvas_size.y/10))
 		for edge_point in $ConstelationContainer.get_children():
 			if edge_point.global_position.x > canvas_size.x - 20 or edge_point.global_position.x < 20 or edge_point.global_position.y > canvas_size.y - 20 or edge_point.global_position.y < 20:
 				star_out = true
-				print('Recalculating position')
 				break
-
+	
+	draw_constellation_line()
+	
+	# Instantiate foreign stars
 	for point in range(50):
 		var star_instance = star_scene.instantiate()
 		add_child(star_instance)
@@ -121,4 +126,17 @@ func get_stars_remaining() -> String:
 	for child in $ConstelationContainer.get_children():
 		count = count + (1 if child.get_selected() else 0)
 	return str(count) + '/' + str($ConstelationContainer.get_child_count())
+	
+func draw_constellation_line():
+	$ConstellationLine.clear_points()
+	for point in constellation_list:
+		$ConstellationLine.add_point(point.g_position)
+
+func _on_star_selectable_select(star_id, selected, g_position) -> void:
+	if selected:
+		constellation_list.append({"star_id":star_id,"g_position":g_position})		
+	else:
+		constellation_list.erase({"star_id":star_id,"g_position":g_position})
+	draw_constellation_line()
+	
 	
--- scripts/star_selectable.gd
+++ scripts/star_selectable.gd
@@ -1,43 +1,35 @@
 extends Node2D
 
-@export var images:Array[CompressedTexture2D]
 var rng = RandomNumberGenerator.new()
-signal select;
+signal select(star_id,selected, g_position);
 var selected:bool
 var idle_animation_frame:int
 var is_clicking = false
 
 func _ready() -> void:
-	if images.size() > 0:
+	
+	if $AnimatedSprite2D.sprite_frames.get_frame_count('idle') > 0:
 		$AnimatedSprite2D.animation = 'idle'
 		idle_animation_frame = rng.randi_range(0,$AnimatedSprite2D.sprite_frames.get_frame_count('idle'))
 		$AnimatedSprite2D.frame = idle_animation_frame
 	else:
-		print('An array of images must be passed!')
+		print('Please add animation frames to idle animation!')
 
-
-func hid_sprite(visible: bool):
-	$AnimatedSprite2D.visible = visible
 
 func get_selected() -> bool:
 	return selected
 	
-
-func _on_select() -> void:
-	$AnimatedSprite2D.animation = 'selected'
-	$AnimatedSprite2D.play()
-
-
-
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed and not is_clicking:
 		if (event.position - global_position).length() < 5:
 			is_clicking = true
 			selected = !selected
 			if selected:
+				select.emit($".".get_instance_id(), true, global_position)
 				$AnimatedSprite2D.animation = 'selected'
 				$AnimatedSprite2D.play()
 			else:
+				select.emit($".".get_instance_id(), false, global_position)
 				$AnimatedSprite2D.stop()
 				$AnimatedSprite2D.animation = 'idle'
 				$AnimatedSprite2D.frame = idle_animation_frame
